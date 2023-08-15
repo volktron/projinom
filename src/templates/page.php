e7438dd7fb4d3bc54ddb4afd68084a177fd2fadb
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-08-15 14:59:11 -0400

    Syntax highlight php code

--- src/templates/page.php
+++ src/templates/page.php
@@ -4,7 +4,13 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title><?=$this->config['name']?></title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/themes/prism-tomorrow.min.css" rel="stylesheet" crossorigin="anonymous">
     <style>
+        .token {
+            text-shadow: none;
+        }
+
         .sidebar {
             position: fixed;
             top: 4em;
@@ -78,7 +84,9 @@
             <?php foreach($versionConfig['directory'] as $section) { ?>
                 <?php foreach($section['pages'] as $page) { ?>
                     <section class="tab-pane fade" id="<?=$page?>">
-                        <?=$section['pageContent'][$page]?>
+                        <div class="container">
+                            <?=$section['pageContent'][$page]?>
+                        </div>
                     </section>
                 <?php } ?>
             <?php } ?>
@@ -90,6 +98,8 @@
             crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/prism.min.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/prism/9000.0.1/components/prism-php.min.js" crossorigin="anonymous"></script>
     <script>
         $('#sidebar .nav-item:first button').trigger('click');
     </script>
