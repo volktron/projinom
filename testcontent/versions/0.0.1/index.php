0efcdb8eb816c9314718046c7f0d07471d3ceb19
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-08-15 08:56:23 -0400

    Test content (partial)

---
+++ testcontent/versions/0.0.1/index.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types=1);
+
+return [
+    'directory' => [
+        [
+            'name' => 'Getting Started',
+            'pages' => [
+                'Introduction',
+                'Setup'
+            ]
+        ],
+        [
+            'name' => 'Tutorial',
+            'pages' => [
+                'Breakfast',
+                'Lunch',
+                'Dinner'
+            ]
+        ]
+    ]
+];
