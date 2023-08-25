4d687902dce55077b6d91e291f725d66c51ff6f6
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-08-24 21:49:08 -0400

    #2 - Reverse sort versions for dropdown usage.

--- src/Builders/Documentation.php
+++ src/Builders/Documentation.php
@@ -8,6 +8,7 @@ class Documentation extends AbstractBuilder
     {
         $versionDirectories = scandir($this->sourcePath . DIRECTORY_SEPARATOR . $this->config['versions_directory']);
         $this->versionDirectories = array_filter($versionDirectories, fn($item) => $item !== '.' && $item !== '..');
+        krsort($this->versionDirectories);
 
         $this->generateContentPage('index');
 
