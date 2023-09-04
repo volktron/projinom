23ca8a1b3b82596e856befafcddc4e5f106dae33
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-09-04 09:55:44 -0400

    Webpack'd third party css

--- js/projinom.js
+++ js/projinom.js
@@ -2,6 +2,11 @@ import $ from "jquery";
 window.$ = $;
 
 import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
 import 'prismjs';
+import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-php'
+import 'prismjs/themes/prism-tomorrow.min.css'
 
 $('#sidebar .nav-item:first button').trigger('click');
