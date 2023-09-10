d85cb11a398696192ba5bb9aa151fec9e1ccbcaa
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-09-09 23:05:30 -0400

    Added sass

--- js/src/projinom.js
+++ js/src/projinom.js
@@ -2,14 +2,16 @@ import $ from "jquery";
 window.$ = $;
 
 import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 import 'prismjs';
-import 'prismjs/components/prism-markup-templating'
-import 'prismjs/components/prism-bash'
-import 'prismjs/components/prism-javascript'
-import 'prismjs/components/prism-typescript'
-import 'prismjs/components/prism-php'
-import 'prismjs/themes/prism-tomorrow.min.css'
+import 'prismjs/components/prism-markup-templating';
+import 'prismjs/components/prism-bash';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-typescript';
+import 'prismjs/components/prism-php';
+import 'prismjs/themes/prism-tomorrow.min.css';
+
+import './projinom.scss';
 
 $('#sidebar .nav-item:first button').trigger('click');
