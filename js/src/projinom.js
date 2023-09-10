de4c73e16d993287423c76d1e267d81cb1d9b28f
Henrik Volckmer <henrik.volckmer@gmail.com>
2023-09-09 20:21:54 -0400

    Updated available languages

--- js/src/projinom.js
+++ js/src/projinom.js
@@ -6,6 +6,9 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 
 import 'prismjs';
 import 'prismjs/components/prism-markup-templating'
+import 'prismjs/components/prism-bash'
+import 'prismjs/components/prism-javascript'
+import 'prismjs/components/prism-typescript'
 import 'prismjs/components/prism-php'
 import 'prismjs/themes/prism-tomorrow.min.css'
 
